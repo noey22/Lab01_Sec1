a7267a447b4e87a8da5426e74deb6f196ffc6112
noey22 <siriyakorn.son@rmutto.ac.th>
2024-08-17 21:05:24 +0700

    Lab001_Sec

---
+++ app/routes/data.tsx
@@ -0,0 +1,62 @@
+export const sculptureList = [
+   {
+     Code: '1',
+     Title: 'ศาสตร์มืดแห่งการชักใยคน',
+     Cover: 'https://storage.naiin.com/system/application/bookstore/resource/product/202407/618115/1000273698_front_XXL.jpg',
+     Category: 'จิตวิทยา การพัฒนาตัวเอง',
+     Author: 'Dr.Hiro',
+     Publishing: 'อมรินทร์ How to',
+     Price: '247.50 บาท',
+     href: 'https://www.naiin.com/product/detail/618115',
+     Bestseller: true,
+     Recommended: true
+   },
+   {
+     Code: '2',
+     Title: 'จิตวิทยาสายดาร์ก',
+     Cover: 'https://storage.naiin.com/system/application/bookstore/resource/product/202304/577274/1000260516_front_XXL.jpg',
+     Category: 'จิตวิทยา การพัฒนาตัวเอง',
+     Author: 'Dr.Hiro',
+     Publishing: 'วีเลิร์น (WeLearn)',
+     Price: '225.00 บาท',
+     href: 'https://www.naiin.com/product/detail/577274',
+     Bestseller: true,
+     Recommended: false
+   },
+   {
+     Code: '3',
+     Title: 'โลกจะยิ้มให้เธอ ในวันที่เธอยิ้มให้ตัวเอง',
+     Cover: 'https://storage.naiin.com/system/application/bookstore/resource/product/202408/619415/1000274452_front_XXL.jpg',
+     Category: 'จิตวิทยา การพัฒนาตัวเอง',
+     Author: 'ทำเรื่องเล่นให้เป็นเรื่องใหญ่',
+     Publishing: 'DOT',
+     Price: '225.00 บาท',
+     href: 'https://www.naiin.com/product/detail/618115',
+     Bestseller: false,
+     Recommended: true
+   },
+   {
+     Code: '4',
+     Title: 'คดีหลอนลิฟต์ซ่อนศพ',
+     Cover: 'https://storage.naiin.com/system/application/bookstore/resource/product/202407/616262/1000273245_front_XXL.jpg',
+     Category: 'นิยายสืบสวนสอบสวน นิยายลี้ลับ',
+     Author: 'ภาคินัย',
+     Publishing: 'prism publishing',
+     Price: '292.50 บาท',
+     href: 'https://www.naiin.com/product/detail/616262',
+     Bestseller: false,
+     Recommended: false
+   },
+   {
+     Code: '5',
+     Title: 'ศพใต้เตียง',
+     Cover: 'https://storage.naiin.com/system/application/bookstore/resource/product/202407/616263/1000273244_front_XXL.jpg',
+     Category: 'นิยายสืบสวนสอบสวน นิยายลี้ลับ',
+     Author: 'สรจักร',
+     Publishing: 'prism publishing',
+     Price: '229.50 บาท',
+     href: 'https://www.naiin.com/product/detail/618115',
+     Bestseller: true,
+     Recommended: true
+   }
+ ];
