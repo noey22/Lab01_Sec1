f63afcdceaeaf019f1ceab9803c98bee1de2251b
noey22 <siriyakorn.son@rmutto.ac.th>
2024-08-19 22:45:40 +0700

    labo1

--- app/routes/BookStatus.tsx
+++ app/routes/BookStatus.tsx
@@ -2,12 +2,12 @@ export default function BookStatus({ Bestseller , Recommended }) {
     return (
       <div className="flex space-x-2">
         {Bestseller && (
-          <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800">
+          <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-emerald-100 text-emerald-800 sm:text-1xl">
             สินค้าขายดี
           </span>
         )}
         {Recommended && (
-          <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+          <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-sky-100 text-sky-800 sm:text-1xl">
             สินค้าแนะนำ
           </span>
         )}
